28173d565e0bd433c207acc5fa9981f595655ae2
Marcos Montero <marcos.montero@gmail.com>
2024-09-04 11:51:04 +0200

    last changes

---
+++ src/api/activitat/services/activitat.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * activitat service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::activitat.activitat');
